3e5df6aec0642bcfeac6eae501bff82fe79918c5
juan jose <chepe0794@hotmail.com>
2016-06-26 21:15:35 -0600

    tarea helpers

---
+++ tarea/HelpersTarea/Helpers/HelpersClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+namespace HelpersTarea.Helpers
+{
+    public static class HelpersClass
+    {
+        public static MvcHtmlString List(this HtmlHelper helper, IEnumerable<string> items)
+        {
+            var result = "<ul>";
+            foreach (var item in items)
+            {
+                result += string.Format("<li>{0}</li>", item);
+            }
+            result += "<ul>";
+            return new MvcHtmlString(result);
+        }
+        public static string Label(string target, string text)
+        {
+            return string.Format("<label for='{0}'>{1}</label>", target, text);
+        }
+        public static string TextArea(string target, string text)
+        {
+            return string.Format("<textarea for='{0}'>{1}</textarea>", target, text);
+
+        }
+
+        public static string CheckBox(string target, string text)
+        {
+            return string.Format("<checkbox for='{0}'>{1}</checkbox>", target, text);
+        }
+
+
+        public static string Textbox(string target, string text)
+        {
+            return string.Format("<textbox for='{0}'>{1}</textbox>", target, text);
+        }
+    }
+}
